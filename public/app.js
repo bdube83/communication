97ee27bce3091aaedfa1b09f59c4d23530871d0a
Bongani Dube <bdube83@gmail.com>
2024-04-30 10:02:58 +0200

    Add common spots to instant messeging

--- public/app.js
+++ public/app.js
@@ -14,17 +14,31 @@ const formLoginDisplay = document.querySelector('.form-login');
 const userConnectionsDropdown = document.querySelector('#userConnections'); // Select the user connections dropdown
 
 function sendMessage(e) {
-    e.preventDefault()
+    e.preventDefault();
     if (nameInput && msgInput.value) {
-        socket.emit('message', {
+        const messageType = 'text'; // Default messageType
+        let messageData = {
             name: nameInput,
             recipientId: recipientUserId,
-            text: msgInput.value
-
-        })
-        msgInput.value = ""
+            text: msgInput.value,
+            messageType: messageType,
+        };
+        // Check if location is enabled
+        if (document.querySelector('#locationCheckbox').checked) {
+            const latitude = parseFloat(document.querySelector('#latitudeInput').value);
+            const longitude = parseFloat(document.querySelector('#longitudeInput').value);
+            // Check if latitude and longitude are valid numbers
+            if (!isNaN(latitude) && !isNaN(longitude)) {
+                messageData.location = [latitude, longitude]; // Set location coordinates
+                messageData.messageType = 'location'; // Update messageType to location
+            } else {
+                console.error('Invalid latitude or longitude input');
+            }
+        }
+        socket.emit('message', messageData);
+        msgInput.value = "";
     }
-    msgInput.focus()
+    msgInput.focus();
 }
 
 function enterRoom(e) {
@@ -47,6 +61,15 @@ msgInput.addEventListener('keypress', () => {
     socket.emit('activity', nameInput)
 })
 
+document.querySelector('#locationCheckbox').addEventListener('change', function() {
+    const locationInputs = document.querySelector('#locationInputs');
+    if (this.checked) {
+        locationInputs.style.display = 'block'; // Show location inputs
+    } else {
+        locationInputs.style.display = 'none'; // Hide location inputs
+    }
+});
+
 userConnectionsDropdown.addEventListener('change', (event) => {
     const selectedUserId = event.target.value;
     socket.emit('getChatHistory', { recipientId: selectedUserId });
@@ -56,7 +79,7 @@ userConnectionsDropdown.addEventListener('change', (event) => {
 // Listen for messages
 socket.on("message", (data) => {
     activity.textContent = ""
-    const { name, text, time } = data
+    const { name, text, time, possibleCommonSpots } = data
     const li = document.createElement('li')
     li.className = 'post'
     if (name === nameInput) li.className = 'post post--right'
@@ -75,6 +98,23 @@ socket.on("message", (data) => {
     }
     document.querySelector('.chat-display').appendChild(li)
 
+    if (possibleCommonSpots && possibleCommonSpots.length > 0) {
+        possibleCommonSpots.forEach(commonSpot => {
+            const li = document.createElement('li');
+            li.className = 'post';
+            if (name === nameInput) {
+                li.classList.add('post--right');
+            } else {
+                li.classList.add('post--left');
+            }
+            li.innerHTML = `<div class="post__header post__header--reply-spot">
+                <span class="post__header--name">Common Spot</span>
+                </div>
+                <div class="post__text">${commonSpot?.name}</div>`
+            chatDisplay.appendChild(li);
+        });
+    }
+
     chatDisplay.scrollTop = chatDisplay.scrollHeight
 });
 
@@ -84,7 +124,7 @@ socket.on("chatHistory", (history) => {
 
     // Display chat history in the UI
     history.forEach(chat => {
-        const { sender, content, createdAt, senderName, recipientName } = chat;
+        const { sender, content, createdAt, senderName, possibleCommonSpots } = chat;
         const li = document.createElement('li');
         li.className = 'post';
         const name = senderName
@@ -104,9 +144,24 @@ socket.on("chatHistory", (history) => {
         <span class="post__header--time">${createdAt}</span> 
         </div>
         <div class="post__text">${content}</div>`
-
         // Append the message to the chat display
         chatDisplay.appendChild(li);
+        if (possibleCommonSpots && possibleCommonSpots.length > 0){
+            possibleCommonSpots.forEach(commonSpot => {
+                const li = document.createElement('li');
+                li.className = 'post';
+                if (sender === currentUserId) {
+                    li.classList.add('post--right');
+                } else {
+                    li.classList.add('post--left');
+                }                li.innerHTML = `<div class="post__header post__header--reply-spot">
+                <span class="post__header--name">Common Spot</span>
+                </div>
+                <div class="post__text">${commonSpot?.name}</div>`
+                // console.log(commonSpot?.name);
+                chatDisplay.appendChild(li);
+            });
+        }
     });
 
     // Scroll to the bottom of the chat display
